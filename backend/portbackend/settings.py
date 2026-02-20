0b7d237f41ac742834afcea57bb4ba0ff8fe5675
Mustapha Haadi <mustaphahaadi04@gmail.com>
2026-02-20 13:16:56 +0000

    feat: Add default SQLite database URL to dj_database_url configuration

--- backend/portbackend/settings.py
+++ backend/portbackend/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=600, default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 DATABASES['default'].update(db_from_env)
 
 # Password validation
